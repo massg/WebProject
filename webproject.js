8a292c42c8bbb08819d0472f8ded359cb54af8d9
Pranesh <ipraneshulleri@gmail.com>
2019-05-30 23:26:16 +0530

    added few routes to the webproject.js

--- webproject.js
+++ webproject.js
@@ -9,4 +9,28 @@ var db = mongojs('mongodb://easygrade:abc123123@cluster0-shard-00-00-txins.mongo
 app.use(express.static('frontend'))
 app.get('/',function(req, res){
 //the main page
+    //res.send("hey yo people")
+    res.sendFile(__dirname+"/frontend/home.html")
+})
+
+app.get('/:college',function(req, res){
+//the page for branch
+    //res.send("hey yo people")
+    res.sendFile(__dirname+"/frontend/branch.html") // this could be ejs lets make it in html it self for now 
+})
+	
+app.get('/branch',function(req, res){
+//the semester page
+    //res.send("hey yo people")
+    res.sendFile(__dirname+"/frontend/semester.html")
+})
+
+app.get('/:semester',function(req, res){
+//the subject page
+    //res.send("hey yo people")
+    res.sendFile(__dirname+"/frontend/subjects.html")
+})	
+
+app.listen(8001, function(){
+console.log("Server Initiated...")
 })
